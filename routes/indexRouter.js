a881219097d23ea9ceae3e3929492e8830365bdc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-10-20 16:43:08 -0400

    Require messages.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,24 +1,7 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-const messages = [
-    {
-        text: "Hi there!",
-        user: "Amando",
-        added: new Date()
-    },
-    {
-        text: "Hello World!",
-        user: "Charles",
-        added: new Date()
-    },
-    {
-        text: "EJS is very interesting, but I prefer react. Lol.",
-        user: "veles",
-        added: new Date()
-    }
-];
-
+const messages = require("../routes/messages");
 
 indexRouter.get("/", (req, res) => {
     res.render("index", { messages: messages });
