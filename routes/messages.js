42b94fa93f3ecca3a456e09ba71a1c67af9db68e
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-10-20 16:41:18 -0400

    Move messages to messages.js

---
+++ routes/messages.js
@@ -0,0 +1,19 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "EJS is very interesting, but I prefer react. Lol.",
+        user: "veles",
+        added: new Date()
+    }
+];
+
+module.exports = messages;
