f8532d3126738651bb0835de48a5c378dcf2deee
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-10-20 16:41:49 -0400

    Create newMessageRouter POST method

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
 const newMessageRouter = Router();
 
+const messages = require("../routes/messages");
+
 newMessageRouter.get("/", (req, res) => {
-    res.render("newMessage");
+    res.render("form");
+});
+
+newMessageRouter.post("/", (req, res) => {
+    const messageText = req.body.newMsg;
+    const messageUser = req.body.newUser;
+
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect("/");
 });
 
 module.exports = newMessageRouter;
