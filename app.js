b8b5f71abcb9f8ae674cadfd1fe713adc63eec6b
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-10-20 16:40:03 -0400

    Add static file access and urlencoded

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
 
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
