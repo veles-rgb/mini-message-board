39b63b680835cba75b0f6e42dbf2ce364f7725e0
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-10-20 18:52:48 -0400

    Add a messages/:id route for individual msgs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,4 +7,9 @@ indexRouter.get("/", (req, res) => {
     res.render("index", { messages: messages });
 });
 
+indexRouter.get("/messages/:id", (req, res) => {
+    const id = req.params.id;
+    res.render("message", { id: Number(id), messages: messages });
+});
+
 module.exports = indexRouter;
